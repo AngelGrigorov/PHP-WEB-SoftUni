12de4bc7664f60d7ba84f976736dc0050f654388
AngelGrigorov <damage9797@gmail.com>
2019-05-28 16:57:07 +0300

    .

--- 04. Database Relations and Design/09. Employee Departments.sql
+++ 04. Database Relations and Design/09. Employee Departments.sql
@@ -6,6 +6,6 @@ FROM employees AS e
 INNER JOIN departments AS d
 ON e.department_id = d.department_id
 WHERE e.salary > 15000
-ORDER BY e.department_id DESC
+ORDER BY d.department_id DESC
 LIMIT 5;
 
