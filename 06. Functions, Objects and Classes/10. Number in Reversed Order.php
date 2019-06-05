2b0da6cbf7a64e01367aa3489b615d5349b1451a
AngelGrigorov <damage9797@gmail.com>
2019-06-05 18:46:54 +0300

    Upload

--- 06. Functions, Objects and Classes/10. Number in Reversed Order.php
+++ 06. Functions, Objects and Classes/10. Number in Reversed Order.php
@@ -1,3 +1,14 @@
 <?php
-$str = readline();
-echo strrev($str);
+class DecimalNumber{
+    public $number;
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+    function printReverseNum($num){
+        $num = strval($num);
+        echo strrev($num);
+    }
+}
+$input = new DecimalNumber(readline());
+$input->printReverseNum($input->number);
