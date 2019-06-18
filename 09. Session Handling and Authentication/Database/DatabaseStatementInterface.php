27a2145784d26d33a6e16956cf45aeaadeb13004
AngelGrigorov <damage9797@gmail.com>
2019-06-18 19:05:09 +0300

    up

--- 09. Session Handling and Authentication/Database/DatabaseStatementInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Database;
-
-interface DatabaseStatementInterface
-{
-    public function execute(array $params = []): DatabaseStatementInterface;
-
-    public function fetch(): \Generator;
-}
